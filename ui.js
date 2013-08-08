f34392dc526fac4e3f1701f78db4f14e3490b365
Will Holcomb <will@dhappy.org>
2013-08-08 14:26:19 -0400

    beginning Freebase integration

--- ui.js
+++ ui.js
@@ -1,6 +1,24 @@
 $( function() {
+
     var $calories = $('<input/>').attr( { type: 'text' } )
-    $('#recipe tbody').append(
+    var $units = $('<select/>')
+    $.ajax( {
+        dataType: 'json',
+        url: 'units.json',
+        async: false,
+        success: function( units ) {
+            options = ''
+            $.each( units, function( typeName, type ) {
+                var $type = $('<optgroup/>').attr( { label: typeName } )
+                $units.append( $type )
+                $.each( type, function( name, abbreviation ) {
+                    $type.append( $('<option/>').val( name ).text( name ) )
+                } )
+            } )
+        }
+    } )
+
+    $('#recipe tbody').prepend(
         $('<tr/>')
             .addClass( 'ingredient' )
             .append(
@@ -9,14 +27,36 @@ $( function() {
                         .suggest( {
                             key: 'AIzaSyDqk53HcVbo2dP8ULc1qANB9Iejr7iXZOg',
                             filter: '(all type:/food/ingredient)'
+                        } )
+                        .bind( 'fb-select', function( evt, data ) {
+                            var envelope = {
+                                query : {
+                                    id: data.id,
+                                    '/food/food/energy': null
+                                }
+                            }
+
+                            console.log( envelope )
+                            $.getJSON( 'http://api.freebase.com/api/service/mqlread?callback=?',
+                                       { query: JSON.stringify( envelope ) },
+                                       function( response ) {
+                                           console.log( response )
+                                           if( response.code == '/api/status/ok' && response.result ) {
+                                               console.log( response.result )
+                                           }
+                                       } )
                         } ) ) )
             .append(
-                $('<td/>').append(
-                    $('<input/>').attr( { type: 'text' } )
-                        .keyup( function() {
-                            $calories.val( $(this).val() )
-                        } )
-                ) )
+                $('<td/>')
+                    .addClass( 'quantity' )
+                    .append(
+                        $('<input/>').attr( { type: 'text' } )
+                            .numeric()
+                            .keyup( function() {
+                                $calories.val( $(this).val() )
+                            } )
+                    )
+                    .append( $units ) )
             .append(
                 $('<td/>').append(
                     $calories
