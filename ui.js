8265b3cebe0194696bdf19fb23e591c9bbc00bdf
Will Holcomb <will@dhappy.org>
2013-08-08 19:28:23 -0400

    adding mayo icon

--- ui.js
+++ ui.js
@@ -2,11 +2,14 @@ $( function() {
 
     function Row() {
         this.$elem = $('<tr/>').addClass( 'ingredient' )
-        var $calories = $('<input/>').attr( { type: 'text' } )
+        this.$suggest = $('<input/>').attr( { type: 'text' } )
+        this.$calories = $('<td/>')
         var $kj = $('<td/>')
         var $icon = $('<td/>').addClass( 'icon' )
         var $quantity = $('<input/>').attr( { type: 'text' } )
         
+        var row = this
+
         var $units = $('<select/>')
         $.ajax( {
             dataType: 'json',
@@ -32,13 +35,14 @@ $( function() {
             .append( $icon )
             .append(
                 $('<td/>').append(
-                    $('<input/>').attr( { type: 'text' } )
+                    this.$suggest
                         .suggest( {
                             key: 'AIzaSyDqk53HcVbo2dP8ULc1qANB9Iejr7iXZOg',
                             filter: '(all type:/food/ingredient)'
                         } )
                         .bind( 'fb-select', function( evt, data ) {
                             $icon.attr( { id: data.name } )
+                            $kj.addClass( 'loading' )
                             
                             var query = {
                                 id: data.id,
@@ -51,6 +55,7 @@ $( function() {
                             
                             $.getJSON( fb_url,
                                        function( response ) {
+                                           $kj.removeClass( 'loading' )
                                            $kj.text( response.result['/food/food/energy'] )
                                            $quantity.keyup()
                                        } )
@@ -65,19 +70,37 @@ $( function() {
                                 var toGrams = new UnitConverter( $(this).val(), $units.val() )
                                 var toCalories = new UnitConverter(
                                     toGrams.as( 'g' ).val() * ( $kj.text() / 100 ), 'kJ' )
-                                $calories.val( toCalories.as( 'kcal' ).val() )
+                                row.$calories.text( toCalories.as( 'kcal' ).val() )
                             } )
                     )
                     .append( $units ) )
             .append( $kj )
-            .append(
-                $('<td/>').append(
-                    $calories
-                ) )
+            .append( this.$calories )
         return this
     }
 
-    $('#recipe tbody').prepend( ( new Row() ).$elem )
-    $('#recipe tbody').prepend( ( new Row() ).$elem )
+    var rows = []
+
+    function addRow() {
+        var row = new Row()
+
+        $('#recipe tbody').append( row.$elem )
+
+        var run = false
+        row.$suggest.keyup( function() {
+            if( ! run ) {
+                run = true
+                addRow()
+            }
+        } )
+
+        row.$calories.change( function() {
+            console.log( arguments )
+        } )
+
+        rows.push( row )
+    }
+
+    addRow()
 } )
    
